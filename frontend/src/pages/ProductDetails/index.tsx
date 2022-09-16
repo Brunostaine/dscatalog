3199616b4094a49a3a152a6d031f3fe2c36d7263
Bruno Staine <brunostaine@hotmail.com>
2022-09-16 13:06:46 -0300

    inicio do product details

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,35 @@
-import './styles.css'
+import "./styles.css";
 
+import { ReactComponent as ArroIcon } from "../../assets/images/arrow.svg";
+import ProductPrice from "../../components/ProductPrice";
 const ProductDetails = () => {
-  return (
-    <div>ProductDetails</div>
-  )
-}
+    return (
+        <div className="product-details-container">
+            <div className="product-details-card">
+                <div className="goback-container">
+                    <ArroIcon />
+                    <h2>Voltar</h2>
+                    <div className="row">
+                        <div className="col-xl-6">
+                            <div className="img-container">
+                                <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="name" />
+                            </div>
+                            <div className="name-price-container">
+                                <h1>Nome produto</h1>
+                                <ProductPrice price={2345.67} />
+                            </div>
+                        </div>
+                        <div className="col-xl-6">
+                            <div className="description-container">
+                                <h2>Descrição do produto</h2>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis eligendi est pariatur perferendis doloribus impedit sunt, tempore incidunt ad sint totam eum aut. Cupiditate quaerat ullam est distinctio soluta maxime?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default ProductDetails
+export default ProductDetails;

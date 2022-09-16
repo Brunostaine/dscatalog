9bed1ea28a0b765b6aa4dab1ce029d84207159ff
Bruno Staine <brunostaine@hotmail.com>
2022-09-16 17:41:15 -0300

    rotas anhinhadas

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Admin from "./pages/Admin";
@@ -7,20 +8,22 @@ import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
 
 const AppRoutes = () => {
+
     return (
         <BrowserRouter>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-            </Routes>
-            <Routes>
+
                 <Route path="/products" element={<Catalog />} />
-            </Routes>
-            <Routes>
+
                 <Route path="/products/:productId" element={<ProductDetails />} />
-            </Routes>
-            <Routes>
-                <Route path="/admin" element={<Admin />} />
+
+                <Route path="/admin" element={<Admin />}>
+                    <Route path="/admin/products" element={<h1>Products</h1>} />
+                    <Route path="/admin/categories" element={<h1>Categoria</h1>} />
+                    <Route path="/admin/usuarios" element={<h1>Usuarios</h1>} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,15 +1,17 @@
-import Navbar from "./Navbar"
-import "./styles.css"
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+
+import "./styles.css";
 
 const Admin = () => {
-  return (
-    <div className="admin-container">
-        <Navbar/>
-        <div className="admin-content">
-            conteudo
+    return (
+        <div className="admin-container">
+            <Navbar />
+            <div className="admin-content">
+                <Outlet />
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Admin
+export default Admin;
